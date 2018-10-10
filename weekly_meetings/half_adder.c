e3b269e29a46ff8cd0da065fd6f425ed8785c2df
Gael Zendejas <gael@BlackBook.local>
2018-10-10 15:31:26 -0400

    added new folder

---
+++ weekly_meetings/half_adder.c
@@ -0,0 +1,45 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int *half_adder(int wire_a, int wire_b) {
+
+    int *sum_carry = (int*) malloc(sizeof *sum_carry);
+    int sum_wire = wire_a ^ wire_b;
+    int carry_wire = wire_a & wire_b;
+
+    *sum_carry = sum_wire;
+    sum_carry++;
+    *sum_carry = carry_wire;
+    sum_carry++;
+    *sum_carry = '\0';
+
+    return sum_carry;
+
+}
+
+int main(int argc, char *argv[]) {
+
+    //Half adder
+    int x_wire = 1;
+    int y_wire = 1;
+
+    //get the sum
+    int sum_wire = x_wire ^ y_wire;
+    //get the carry
+    int carry_wire = x_wire & y_wire;
+
+    printf("The sum is: %d\nThe carry is: %d\n", sum_wire, carry_wire);
+
+    int *temp = half_adder(0, 1);
+
+    temp -= 2;
+
+    sum_wire = *temp;
+    temp++;
+    carry_wire = *temp;
+
+    printf("The sum is: %d\nThe carry is: %d\n", sum_wire, carry_wire);
+
+    return 0;
+
+}
