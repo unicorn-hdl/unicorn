8515e5c6a4bb4c832425a14cd154d7ff66bddde8
David Lalo <davidlalo@Davids-MacBook-Pro.local>
2018-10-24 21:49:42 -0400

    We've done the Ast, I think?

--- Code/Ast.ml
+++ Code/Ast.ml
@@ -1,2 +1,27 @@
-type op = Add | Sub
-type prim = Not | And | Or | Nand | Nor | Xor | Xnor
+type intOp = Add | Sub
+type boolOp = And | Or | Nand | Nor | Xor | Xnor
+type unOp = Not
+type typ = Int | Bus
+type bind = typ * string
+
+type intExpr = 
+      Literal of int
+    | Id of string
+    | IntBinop of intExpr * intOp * intExpr
+
+and type binExpr = 
+      Buslit of bool list
+    | Id of string
+    | BoolBinop of binExpr * binOp * binExpr | Unop of unOp * binExpr
+    | Assign of string * binExpr
+    | Call of string * binExpr list
+    | For of Id * intExpr * intExpr
+    | Noexpr
+
+type module_decl = { typ    : int list;
+					mname   : string;
+					formals : bind list;
+					locals  : bind list;
+					body    : binExpr list; }
+
+type program = bind list * module_decl list
