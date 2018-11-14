b44da7d3d37cdd4f1d82f508ac9f107cb2698810
David Lalo <davidlalo@Davids-MacBook-Pro.local>
2018-11-14 13:20:14 -0500

    EVERYTHING WORKS

--- Code/codegen.ml
+++ Code/codegen.ml
@@ -20,7 +20,7 @@ let translate (inlist,  binlist, outlist) =
   and void_t     = L.void_type   context in
   
   let mainfn_typ = L.function_type void_t [|i32_t|] in 
-  let the_main_function = L.define_function "mainfn" mainfn_typ the_module in
+  let the_main_function = L.define_function "main" mainfn_typ the_module in
   let builder = L.builder_at_end context (L.entry_block the_main_function) in
 
   let printf_t: L.lltype =
@@ -34,16 +34,24 @@ let translate (inlist,  binlist, outlist) =
     | SBuslit b -> ignore(print_endline ("lit " ^ b));L.const_int i32_t (int_of_string ("0b" ^ b))
     | SPrint (str, e) -> ignore(print_string ("print " ^ str)); 
     (* The bit for dealing with strings is fussy bc no built-in string_type*)
+    (*
           L.build_call printf_func 
                        [|L.const_string context (str ^ ": ")|] 
                        "printf" builder;
+                       *)
           L.build_call printf_func 
                        [|int_format_str; expr e|] 
                        "printf" builder
     | _ -> ignore (print_endline "nomatch! "); L.const_int i32_t 0
   in  
 
+  (*
   let builder = L.block_terminator (L.insertion_block builder) in
-  ignore(List.map expr binlist)
+*)
+  ignore(print_endline "this step is happenind");
+  ignore(List.map expr binlist);
+  ignore(L.build_ret_void (L.builder_at_end context (L.entry_block the_main_function)));
+  ignore(L.block_terminator (L.insertion_block builder));
+  ignore(print_endline "hello")
 );
  the_module
