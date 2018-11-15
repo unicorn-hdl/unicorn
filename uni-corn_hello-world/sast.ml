e83e0ab183d1788d8e65bb5c9b71026dd21c97ba
Gael <g.zendejas@columbia.edu>
2018-11-14 20:14:01 -0500

    Final v0 of directory to submit for Hello World

---
+++ uni-corn_hello-world/sast.ml
@@ -0,0 +1,44 @@
+open Ast
+
+type hardBind = int * string
+type hardmd = {outlistH: hardBind list;
+               nameH: string;
+               formalsH: hardBind list;
+               localsH: hardBind list;
+               linelistH: binExpr list}
+
+type sintExpr  = int * sintExpr2
+and  sintExpr2 = 
+      SLit of int
+    | SIntId of string
+    | SIntBinop of sintExpr * intOp * sintExpr
+
+type sbinExpr  = int * sbinExpr2
+and  sbinExpr2 = 
+      SBuslit of string (*since lex is annoying about turning 01011 into [0;1;0;1;1] keep as string until later down the line*)
+    | SBoolId of string
+    | SBoolBinop of sbinExpr * boolOp * sbinExpr
+    | SUnop of unOp * sbinExpr
+    | SAssign of bool * sbinExpr * sbinExpr * bool
+    | SIndex of sbinExpr * srange
+    (*when bool = false, normal; bool = true, register*)
+    (*final bool is init state*)
+    (*add new assign indexing rule to LRM*)
+    | SPrint of string * sbinExpr
+    | SCall of string * sbinExpr list
+    | SFor of string * srange * sbinExpr list
+    | SNoexpr
+
+and srange = SRange of sintExpr * sintExpr
+
+(*Important: Ast does not contatin a "locals" bind list, but this info is contained in the lineList. It must be extracted and put in the locals list here, when semant is building smds*)
+type smd = {outlist: hardBind list;
+            name: string;
+            formals: hardBind list;
+            locals: hardBind list;
+            linelist: sbinExpr list}
+
+type sprogram = (*bind list *  *) smd list
+
+let string_of_sprogram = function
+| _ -> "this"
---
+++ uni-corn_hello-world/testall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Testing solely for 'Hello World' deliverable
+ocamlbuild -use-ocamlfind -pkgs llvm,llvm.analysis -cflags -w,+a-4 unic.native
+
+touch print_test.ll
+
+./unic.native print_test.uni > print_test.ll
+
+/usr/local/Cellar/llvm/7.0.0/bin/llc -relocation-model=pic print_test.ll
+
+CC -o print_test print_test.s
+
+chmod 755 print_test
+
+./print_test
