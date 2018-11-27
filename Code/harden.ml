fd2899f01c10a9ebdf08baf75b5170f2f405541f
David Lalo <davidlalo@dyn-160-39-137-219.dyn.columbia.edu>
2018-11-27 05:15:31 -0500

    Set up pointer table, and it compiles, but seems to be returning empty table

--- Code/harden.ml
+++ Code/harden.ml
@@ -2,26 +2,35 @@ open Ast
 open Printer
 module StringMap = Map.Make(String)
 
-let nl = []
-(*(* lookup table for finding variables *)
-let lookup n = try StringMap.find n local_vars
-				with Not_Found -> raise (Failure ("unrecognized function " ^ n))
-in
+let modzIntoTuples d m = List.map (fun d-> (d,m)) d
 
-let _ = find_func "main" in (* Ensure "main" is defined *)
-in
-(* Build local symbol table of variables for this function *)
-let symbols = List.fold_left (fun m (ty, name) -> StringMap.add name ty m)
-	            StringMap.empty (globals @ func.formals @ func.locals )
-in
+(* make a table for which modules call a given module*)
 
-let check_function func =
-    (* Make sure no formals or locals are void or duplicates *)
-    check_binds "formal" func.formals;
-    check_binds "local" func.locals;
-*)
-(* expression builder: looks at expressions and tells compiler what to make that into for LLVM to understand 
-first match sees integers and tells LLVM it is a 32 bit val int with value i *)
+let rec isACall ls = function
+    | Buslit(x) -> ls
+    | BoolId(x) -> ls
+    | BoolBinop(l, op, r) -> isACall ls l @ ls @ isACall ls r
+    | Unop(op, exp) -> isACall ls exp @ ls
+    | Assign(a, e1, e2, b) -> isACall ls e1 @ ls @ isACall ls e2
+    | Index(e, r) -> isACall ls e @ ls
+    | Print(s, e) -> isACall ls e @ ls
+    | For(s, r, e) -> getlines e @ ls 
+    | ModExpr(Module_decl(o,name,f,exprs)) -> if List.mem name ls 
+                        then ls 
+                        else name :: getlines exprs @  ls
+    | Noexpr -> ls 
+    | _ -> ls
+(*
+and isACallOnTuples (exprs, ls) = isACall ls exprs
+and runThroughLines ls exprs = List.fold_left isACallOnTuples (modzIntoTuples exprs ls)
+this is similar to get lines, but more complicated*)
+and getlines lines = List.fold_left isACall [] lines
+
+let buildTable m (Module_decl(out, name, formal, lines)) = StringMap.add name (getlines lines) m
+let table mdlist = List.fold_left buildTable StringMap.empty mdlist
+
+(*--------------------------------------------------*)
+
 let extract (Lit(x)) = x
 
 let rec eval = function
@@ -44,15 +53,21 @@ let hardenmd (Module_decl(outs, name, formals, linelist)) =
 let harden ast = 
 	List.map hardenmd ast
 
-
+(*This is just for printing ---------------------------*)
 let callx x = ( Call(x, []) )
 let modA = Module_decl([], "modA", [], [])
 let bExprs = 
-        [Index(Buslit("00100"), Range(IntBinop((Lit(4)), Sub, Lit(1)), Lit(3)))]
+        [Index(Buslit("00100"), Range(IntBinop((Lit(4)), Sub, Lit(1)), Lit(3)));
+        Print("printname", callx "modA")]
 let modB = Module_decl([], "modB", [], bExprs)
 let mdlistEx = [modA;
                 modB
                ]
 
 let toString (Module_decl(a,b,c,d)) = b ^ "\n" ^ toStringBinExprlist d;;
-List.iter (fun x -> print_endline (toString x)) (harden mdlistEx)
+List.iter (fun x -> print_endline (toString x)) (harden mdlistEx);;
+let printfun key v = print_string(key ^ ": ");
+                     print_string (string_of_int(List.length v));
+                     List.iter print_string v; print_endline "";;
+print_endline("table values");;
+StringMap.iter printfun (table mdlistEx)
