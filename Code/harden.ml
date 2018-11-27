62aadd1399bf2ba14c96649c78e44e8d2faf33e7
David Lalo <davidlalo@dyn-160-39-137-219.dyn.columbia.edu>
2018-11-27 03:22:56 -0500

    Hardening works but is missing variables and typechecking

--- Code/harden.ml
+++ Code/harden.ml
@@ -1,5 +1,5 @@
-
-module A = Ast
+open Ast
+open Printer
 module StringMap = Map.Make(String)
 
 let nl = []
@@ -22,22 +22,37 @@ let check_function func =
 *)
 (* expression builder: looks at expressions and tells compiler what to make that into for LLVM to understand 
 first match sees integers and tells LLVM it is a 32 bit val int with value i *)
-let rec hardenline = function
-	| Index(expr, Range(a,b)) -> Index(expr, Range(eval a, eval b))
-	| For (index, Range(a,b), exprs) -> For(index, Range(eval a, eval b), hardenline exprs)
-	| a -> a
+let extract (Lit(x)) = x
 
 let rec eval = function
-	 | Lit(x) -> x
+	 | Lit(x) -> Lit(x)
 	 | IntBinop(lval, op, rval) -> if op = Add 
-	 				then eval lval + eval rval 
-					else eval lval - eval rval
-	 | IntId(name) -> 1 (*this must be done*)
+	 				then Lit(extract (eval lval) + extract (eval rval))
+					else Lit(extract (eval lval) - extract (eval rval))
+	 | IntId(name) -> Lit(1) (*this must be done*)
+
+let rec hardenline = function
+	| Index(expr, Range(a,b)) -> Index(expr, Range(eval a, eval b))
+	| For(index, Range(a,b), exprs) -> For(index, Range(eval a, eval b), List.map hardenline exprs)
+	| a -> a
 
 let hardenarg (expr, name) = (eval expr, name)
 
-let hardenmd Module_decl(outs, name, formals, linelist) =
+let hardenmd (Module_decl(outs, name, formals, linelist)) =
 	Module_decl(List.map hardenarg outs, name, List.map hardenarg formals, List.map hardenline linelist)
 
 let harden ast = 
 	List.map hardenmd ast
+
+
+let callx x = ( Call(x, []) )
+let modA = Module_decl([], "modA", [], [])
+let bExprs = 
+        [Index(Buslit("00100"), Range(IntBinop((Lit(4)), Sub, Lit(1)), Lit(3)))]
+let modB = Module_decl([], "modB", [], bExprs)
+let mdlistEx = [modA;
+                modB
+               ]
+
+let toString (Module_decl(a,b,c,d)) = b ^ "\n" ^ toStringBinExprlist d;;
+List.iter (fun x -> print_endline (toString x)) (harden mdlistEx)
