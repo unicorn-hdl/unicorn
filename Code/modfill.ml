3ebca4c5f3f001834c78b7c1a92dc598d8adf9fd
djlalo08 <djlalo08@gmail.com>
2018-11-27 01:16:20 -0500

    Initial Modfill. Must do the actual changing part

---
+++ Code/modfill.ml
@@ -0,0 +1,37 @@
+open Ast
+module StringMap = Map.Make(String)
+
+let actonlines newmodlist = newmodlist 
+
+let populateMap m (Module_decl(a,b,c,d), e) = StringMap.add b (Module_decl(a,b,c,d)) m
+
+let replaceCalls ((Module_decl(a, b, c, d)), m) =
+                        ((Module_decl(a, b, c, actonlines d)), m)
+
+let callx x = (Call(x, []))
+let modx x = (Module_decl([], x, [], []))
+let e = StringMap.empty
+
+let mdlistEx = [((modx "modA"), e);
+                ((modx "modB"), e)
+               ]
+
+(*
+let mdlistEx = [(1,2); (3,2); (5,12)]
+*)
+
+(*md list-> md list*)
+let createMapz mdlist= List.fold_left populateMap StringMap.empty mdlist
+let call mdlist = List.map replaceCalls mdlist
+
+let theMap = createMapz mdlistEx
+let x = call mdlistEx
+
+let toString (Module_decl(a,b,c,d)) = b
+let printx (x,y) = print_endline(toString x)
+let printz x = List.iter printx x;;
+let printMapEl key v = print_endline(key ^ ": " ^ (toString v))
+let printMap m = StringMap.iter printMapEl m;;
+
+printz x;;
+printMap theMap 
