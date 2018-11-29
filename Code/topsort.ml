d89594774fe70b9c54c7a334ecf4fb1563295287
begreative <be.greative@gmail.com>
2018-11-28 19:20:39 -0500

    Added topsort pseudocode and first steps

---
+++ Code/topsort.ml
@@ -0,0 +1,50 @@
+open Ast
+
+(* 
+
+  NETLIST (Stuff that happens before)
+  - Receive list of pattern-matched tupples of type binExpr or UnExpr
+
+1. take netlist and ensure the tuple form (md * [listOfExprs]) to tie
+the module to the netlist expression being sorted
+
+2. Create a new list of tuples of kind (md * [listOfMds])
+
+3. iterate through listOfExprs, if md found, add to listOfMds of that md
+
+4. Using list of tuples of (md * listOfMds), extract the operator
+
+5. Apply the following in-degrees to the list objects based on the operator:
+---> (in, gates (order of modules? arbitrary?), outs)
+
+6. Output of topsort should go into nomodules
+
+*)
+
+open Sast
+
+let unordered_modules = [.md * A.md::tail]
+
+let ordered_modules =
+  let f (modules, indegree) = 
+    (ordered_modules,
+     List.filter (fun op -> op <> modList) op) in
+  List.map op modList
+ 
+let modList =  
+  uniques (List.flatten(List.map (fun (op, indegree) -> modList::tail) indegree))
+ 
+let get_indegree lib =
+  try (List.assoc modList indegree)
+  (* incomplete *)
+
+(* pretty straightforward algorith goes here *)
+ 
+let () =
+  print_string "result: \n ";
+  print_endline (String.concat ", " res);
+;;
+
+
+
+
