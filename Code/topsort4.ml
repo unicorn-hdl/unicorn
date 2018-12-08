50fc4d26fa2790234e0833c2597dd2fe78b5c86e
David Lalo <davidlalo@dyn-160-39-137-245.dyn.columbia.edu>
2018-12-08 03:53:47 -0500

    Whoops! These are sort of important. Forgot to add them

---
+++ Code/topsort4.ml
@@ -0,0 +1,62 @@
+(*NOTE much of this topsort code is not our own. 
+ * We just wrote interfacing with our netlist for it.
+ * All credit goes to RosettaCode.org*)
+
+open Ast
+module StringMap = Map.Make(String)
+
+
+let topsort netlist = 
+    let globs = snd netlist in
+    let netlist = ("","","","")::(fst netlist) in 
+    let foldFn outMap (a,b,c,d) = StringMap.add a (a,b,c,d) outMap in
+    let map = List.fold_left foldFn StringMap.empty netlist in
+    let lookup x = match x with
+       | "0" | "0b" | "1" | "1b" -> ("","","","")
+       | _ ->
+            if StringMap.mem x map
+            then StringMap.find x map
+            else let _ = print_endline("missed in topsort: "^x) in ("","","","") in
+    let foldFn2 lst (a,b,c,d) = 
+            if (c=d)
+            then  ((a,b,c,d), [lookup c])::lst
+            else ((a,b,c,d), [lookup c; lookup d])::lst in
+    let dep_libs = List.fold_left foldFn2 [] netlist in
+
+
+(*Code here and beyond is not ours*)
+let dep_libs =
+  let f (lib, deps) =  (* remove self dependency *)
+    (lib,
+     List.filter (fun d -> d <> lib) deps) in
+  List.map f dep_libs in
+ 
+let rev_unique =
+  List.fold_left (fun acc x -> if List.mem x acc then acc else x::acc) [] in
+ 
+let libs =  (* list items, each being unique *)
+  rev_unique (List.flatten(List.map (fun (lib, deps) -> lib::deps) dep_libs)) in
+ 
+let get_deps lib =
+  try (List.assoc lib dep_libs)
+  with Not_found -> [] in
+ 
+let res =
+  let rec aux acc later todo progress =
+  match todo, later with
+  | [], [] -> (List.rev acc)
+  | [], _ ->
+      if progress
+      then aux acc [] later false
+      else invalid_arg "un-orderable data"
+  | x::xs, _ ->
+      let deps = get_deps x in
+      let ok = List.for_all (fun dep -> List.mem dep acc) deps in
+      if ok
+      then aux (x::acc) later xs true
+      else aux acc (x::later) xs progress
+  in
+  let starts, todo = List.partition (fun lib -> get_deps lib = []) libs in
+  aux starts [] todo false  in
+
+(res, globs)
---
+++ Code/topsort5.ml
@@ -0,0 +1,74 @@
+(*NOTE much of this topsort code is not our own. 
+ * We just wrote interfacing with our netlist for it.
+ * All credit goes to RosettaCode.org*)
+
+open Ast
+module StringMap = Map.Make(String)
+
+
+let topsort netlist = 
+    let getStr = function
+        | Index(BoolId(x),_) -> x
+        | BoolId(x) -> x in
+    let foldFn outMap expr = match expr with
+        | Assign(_,x,_,_) -> StringMap.add (getStr x) expr outMap
+        | Print(x,_) -> StringMap.add x expr outMap in
+    let map = List.fold_left foldFn StringMap.empty netlist in
+    let dependencies expr = 
+        let rval =
+            match expr with
+            | Assign(_,_,x,_) -> x
+            | Print(_,x) -> x in
+        match rval with
+        | Buslit(x) -> []
+        | BoolId(x) -> print_endline("1: "^x); [StringMap.find x map]
+        | BoolBinop(l,op,r) -> print_endline("2"); [StringMap.find (getStr l) map; 
+                                StringMap.find (getStr r) map]
+        | Unop(op,ex) -> print_endline("3");[StringMap.find (getStr ex) map]
+        | Assign(_,_,_,_) -> print_endline("a thing should never happen did");[]
+        | Index(ex,_) -> print_endline("4: "^Printer.getBinExpr ex);[StringMap.find (getStr ex) map]
+        | Print(_,_) -> print_endline("a thing that should never happen did");[]
+        | Call(_,_) -> print_endline("a thing that should never happen did");[] 
+        | For(_,_,_) -> print_endline("a thing that should never happen did");[]
+        | ModExpr(_,_,_) -> print_endline("a thing that should never happen did");[]
+        | Noexpr -> [] in
+
+    let foldFn2 outList expr = (expr, (dependencies expr))::outList in
+    let dep_libs = List.fold_left foldFn2 [] netlist in
+
+(*Code here and beyond is not ours*)
+let dep_libs =
+  let f (lib, deps) =  (* remove self dependency *)
+    (lib,
+     List.filter (fun d -> d <> lib) deps) in
+  List.map f dep_libs in
+ 
+let rev_unique =
+  List.fold_left (fun acc x -> if List.mem x acc then acc else x::acc) [] in
+ 
+let libs =  (* list items, each being unique *)
+  rev_unique (List.flatten(List.map (fun (lib, deps) -> lib::deps) dep_libs)) in
+ 
+let get_deps lib =
+  try (List.assoc lib dep_libs)
+  with Not_found -> [] in
+ 
+let res =
+  let rec aux acc later todo progress =
+  match todo, later with
+  | [], [] -> (List.rev acc)
+  | [], _ ->
+      if progress
+      then aux acc [] later false
+      else invalid_arg "un-orderable data"
+  | x::xs, _ ->
+      let deps = get_deps x in
+      let ok = List.for_all (fun dep -> List.mem dep acc) deps in
+      if ok
+      then aux (x::acc) later xs true
+      else aux acc (x::later) xs progress
+  in
+  let starts, todo = List.partition (fun lib -> get_deps lib = []) libs in
+  aux starts [] todo false  in
+
+res
