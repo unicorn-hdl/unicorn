9eb162b871f30299f5d435ad100418ac8ffb10dd
begreative <be.greative@gmail.com>
2018-12-14 13:29:40 -0500

    First step of restructuring test-suite

--- Code/testall.sh
+++ Code/testall.sh
@@ -5,10 +5,10 @@ ocamlbuild -use-ocamlfind -pkgs llvm,llvm.analysis -cflags -w,+a-4 unic.native
 
 touch print_test.ll
 
-./unic.native tests/new.uni > print_test.ll
+./unic.native tests/new.uni > ./tests/print_test.ll
 
-/usr/local/Cellar/llvm/7.0.0/bin/llc -relocation-model=pic print_test.ll
+/usr/local/Cellar/llvm/7.0.0/bin/llc -relocation-model=pic ./tests/print_test.ll
 
-CC -o print_test print_test.s
+CC -o ./tests/print_test ./tests/print_test.s
 
 ./print_test
