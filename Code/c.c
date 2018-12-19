2ec50f480f819e123b54d047572612faefe1f645
David Lalo <davidlalo@Davids-MacBook-Pro.local>
2018-12-19 10:52:10 -0500

    c.c

--- Code/c.c
+++ Code/c.c
@@ -2,15 +2,44 @@
 
 void tick();
 
+extern int sel_0;
+extern int sel_1;
+extern int sel_2;
+
 int main(){
-	printf("tick1\n");
+	printf("ALU inputs are\nA: 00111111(63))\nB: 11000100(-60)\n\n");
+
+	printf("Add(00000011)\n");
 	tick();
-	printf("\ntick2\n");
+
+	printf("\nSub(01111011)\n");
+	sel_2 = 0;
+	sel_1 = 0;
+	sel_0 = 1;
 	tick();
-	printf("\ntick3\n");
+
+	printf("\nLogical And(00000100)\n");
+	sel_2 = 1;
+	sel_1 = 0;
+	sel_0 = 0;
 	tick();
-	printf("\ntick4\n");
+
+	printf("\nLogical Or(11111111)\n");
+	sel_2 = 1;
+	sel_1 = 0;
+	sel_0 = 1;
 	tick();
-	printf("\ntick5\n");
+
+	printf("\nLogical Xor(11111011)\n");
+	sel_2 = 1;
+	sel_1 = 1;
+	sel_0 = 0;
+	tick();
+
+	printf("\nNot A(11000000)\n");
+	sel_2 = 1;
+	sel_1 = 1;
+	sel_0 = 1;
 	tick();
 }
+
